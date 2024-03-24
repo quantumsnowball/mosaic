08747158bf7d7f37e158fe996959253f92e6cb54
Quantum Snowball <quantum.snowball@gmail.com>
2024-03-24 14:54:19 +0800

    basic command assemble

--- mosaic/cli.py
+++ mosaic/cli.py
@@ -10,7 +10,29 @@ def mosaic() -> None:
 
 
 @mosaic.command()
-def remove() -> None:
-    cwd = Path(__file__).parent
-    deepmosaic_path = cwd / 'DeepMosaics' / 'deepmosaic.py'
-    subprocess.run(['python', deepmosaic_path])
+@click.option('-i', '--input', required=True, type=str, help='input media path')
+@click.option('--model_path', default=None, type=str, help='pretrained model path')
+@click.argument('output', required=True, type=str)
+def remove(input: str,
+           output: str,
+           model_path: str | None) -> None:
+    # path
+    working_dir = Path(__file__).parent
+    deepmosaic_path = str(working_dir / 'DeepMosaics' / 'deepmosaic.py')
+    media_path = input
+    result_dir = str(Path(output).parent)
+    default_model_path = str(working_dir/'DeepMosaics'/'pretrained_models'/'clean_youknow_video.pth')
+    model_path = model_path if model_path is not None else default_model_path
+
+    # args
+    args = []
+    args += ['--mode', 'clean']
+    args += ['--no_preview']
+    args += ['--media_path', media_path]
+    args += ['--result_dir', result_dir]
+    args += ['--model_path', model_path]
+
+    # run
+    command = ['python', deepmosaic_path] + args
+    print(' '.join(command))
+    # subprocess.run(command)
