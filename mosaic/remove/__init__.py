1dbfa130bb99b6e21cf657ba2f8fb7345c1374f7
Quantum Snowball <quantum.snowball@gmail.com>
2024-03-24 19:46:54 +0800

    Command as dataclass

--- mosaic/remove/__init__.py
+++ mosaic/remove/__init__.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import click
 
+from mosaic.remove.command import DeepMosaicsCommand
 from mosaic.utils import HMS, HMSParamType
 
 
@@ -24,42 +25,21 @@ def remove(input: str,
            dry_run: bool,
            output: str,
            ) -> None:
-    #
-    # path
-    #
-    root_package_dir = Path(__file__).parent.parent
-    deepmosaic_path = str(root_package_dir / 'DeepMosaics' / 'deepmosaic.py')
-    media_path = input
-    result_dir = str(Path(output).parent)
-    default_model_path = str(root_package_dir/'DeepMosaics'/'pretrained_models'/'clean_youknow_video.pth')
-    model_path = model_path if model_path is not None else default_model_path
+    command = DeepMosaicsCommand(
+        root=Path(__file__).parent.parent,
+        media_path=input,
+        start_time=start_time,
+        end_time=end_time,
+        preview=preview,
+        result_dir=str(Path(output).parent),
+        model_path=model_path
+    )
 
-    #
-    # args
-    #
-    args = []
-    args += ['--mode', 'clean']
-    if not preview:
-        args += ['--no_preview']
-    args += ['--media_path', media_path]
-    if start_time and end_time:
-        if not end_time > start_time:
-            click.echo('Invalid start time or end time')
-        last_time = str(end_time - start_time)
-        args += ['--start_time', str(start_time)]
-        args += ['--last_time', str(last_time)]
-    args += ['--result_dir', result_dir]
-    args += ['--model_path', model_path]
-
-    #
-    # run
-    #
-    command = ['python', deepmosaic_path] + args
-    click.echo(' '.join(command))
+    click.echo(str(command))
     if dry_run:
         return
     # run in process group
-    proc = subprocess.Popen(command, preexec_fn=os.setsid)
+    proc = subprocess.Popen(command.tokens, preexec_fn=os.setsid)
     try:
         proc.wait()
     except KeyboardInterrupt:
