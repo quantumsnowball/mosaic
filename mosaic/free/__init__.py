aae1d81c1701f0f3bb29a1528e2006688f4cde2c
Quantum Snowball <quantum.snowball@gmail.com>
2024-03-25 13:54:54 +0800

    command basic args

--- mosaic/free/__init__.py
+++ mosaic/free/__init__.py
@@ -1,6 +1,25 @@
+from pathlib import Path
+
 import click
+
+from mosaic.utils import HMS, HMSParamType, VideoPathParamType
 
 
 @click.command()
-def free() -> None:
+@click.option('-i', '--input', required=True, type=VideoPathParamType(), help='input media path')
+@click.option('-ss', '--start-time', default=None, type=HMSParamType(), help='start time in HH:MM:SS')
+@click.option('-to', '--end-time', default=None, type=HMSParamType(), help='end time in HH:MM:SS')
+@click.option('--model-file', default=None, type=VideoPathParamType(), help='pretrained model path')
+@click.argument('output-file', required=True, type=VideoPathParamType())
+def free(input: Path,
+         start_time: HMS | None,
+         end_time: HMS | None,
+         model_file: Path | None,
+         output_file: Path,
+         ) -> None:
     click.echo('mosaic free')
+    print(input)
+    print(start_time)
+    print(end_time)
+    print(model_file)
+    print(output_file)
