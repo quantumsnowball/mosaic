41b054e5f3fa0fc587774470bf04504c30a27a3a
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-18 05:13:20 +0800

    simplify

--- mosaic/free/cleaner/processor/remove.py
+++ mosaic/free/cleaner/processor/remove.py
@@ -8,13 +8,6 @@ from mosaic.free.cleaner.processor import utils
 from mosaic.free.net.netG.BVDNet import BVDNet
 
 
-def to_tensor(data, gpu_id):
-    data = torch.from_numpy(data)
-    if gpu_id != '-1':
-        data = data.cuda()
-    return data
-
-
 def normalize(data):
     '''
     normalize to -1 ~ 1
@@ -43,11 +36,11 @@ def remove_mosaic(x: int, y: int, size: int,
 
     input_stream = np.array(input_stream).reshape(
         1, T, INPUT_SIZE, INPUT_SIZE, 3).transpose((0, 4, 1, 2, 3))
-    input_stream = to_tensor(
-        normalize(input_stream), gpu_id=gpu_id)
+
+    input_stream_tensor = torch.from_numpy(normalize(input_stream)).cuda()
 
     with torch.no_grad():
-        unmosaic_pred = netG(input_stream, previous_frame)
+        unmosaic_pred = netG(input_stream_tensor, previous_frame)
 
     img_fake = utils.tensor2im(unmosaic_pred, rgb2bgr=True)
     previous_frame = unmosaic_pred
