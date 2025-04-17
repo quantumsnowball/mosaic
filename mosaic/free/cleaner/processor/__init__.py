e52aeeadba12369b9151b89e7e7cef5c9aacdb7e
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-18 02:03:14 +0800

    refactor, move fn to remove as module

--- mosaic/free/cleaner/processor/__init__.py
+++ mosaic/free/cleaner/processor/__init__.py
@@ -1,59 +1,17 @@
 import os
-import time
 from pathlib import Path
 from queue import Queue
 from threading import Thread
 from typing import Self
 
-import cv2
 import numpy as np
-import torch
 
 from mosaic.free.cleaner import runmodel
 from mosaic.free.cleaner.packer import Package, Packer
+from mosaic.free.cleaner.processor.remove import remove_mosaic
 from mosaic.free.net.netG.BVDNet import BVDNet
 from mosaic.free.net.netM.BiSeNet import BiSeNet
-from mosaic.free_old.utils import data
 from mosaic.free_old.utils import image_processing as impro
-
-N, T, S = 2, 5, 3
-LEFT_FRAME = (N*S)        # 6
-POOL_NUM = LEFT_FRAME*2+1  # 13
-INPUT_SIZE = 256
-FRAME_POS = np.linspace(0, (T-1)*S, T, dtype=np.int64)
-
-
-def remove_mosaic(x: int, y: int, size: int,
-                  previous_frame: torch.Tensor | None,
-                  *,
-                  p: Package,
-                  netG: BVDNet,
-                  gpu_id: int = 0) -> tuple[torch.Tensor | None,
-                                            np.ndarray,
-                                            np.ndarray]:
-    img_origin = p.img_origin
-    img_pool = p.img_pool
-    input_stream = []
-    for pos in FRAME_POS:
-        input_stream.append(impro.resize(
-            img_pool[pos][y-size:y+size, x-size:x+size], INPUT_SIZE, interpolation=cv2.INTER_CUBIC)[:, :, ::-1])
-
-    if previous_frame is None:
-        previous_frame = data.im2tensor(
-            input_stream[N], bgr2rgb=True, gpu_id=str(gpu_id))
-
-    input_stream = np.array(input_stream).reshape(
-        1, T, INPUT_SIZE, INPUT_SIZE, 3).transpose((0, 4, 1, 2, 3))
-    input_stream = data.to_tensor(
-        data.normalize(input_stream), gpu_id=gpu_id)
-
-    with torch.no_grad():
-        unmosaic_pred = netG(input_stream, previous_frame)
-
-    img_fake = data.tensor2im(unmosaic_pred, rgb2bgr=True)
-    previous_frame = unmosaic_pred
-
-    return (previous_frame, img_origin.copy(), img_fake.copy())
 
 
 class Processor:
@@ -122,9 +80,8 @@ class Processor:
                 img_output = impro.replace_mosaic(
                     img_origin, img_fake, mask, x, y, size, no_feather=False)
 
-                # out_bytes = package.img_origin.astype(np.uint8).tobytes()
+                # write bytes to output
                 out_bytes = img_output.astype(np.uint8).tobytes()
-
                 output.write(out_bytes)
 
     def run(self) -> None:
---
+++ mosaic/free/cleaner/processor/remove.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import torch
+
+from mosaic.free.cleaner.packer import Package
+from mosaic.free.net.netG.BVDNet import BVDNet
+from mosaic.free_old.utils import data
+from mosaic.free_old.utils import image_processing as impro
+
+N, T, S = 2, 5, 3
+LEFT_FRAME = (N*S)        # 6
+POOL_NUM = LEFT_FRAME*2+1  # 13
+INPUT_SIZE = 256
+FRAME_POS = np.linspace(0, (T-1)*S, T, dtype=np.int64)
+
+
+def remove_mosaic(x: int, y: int, size: int,
+                  previous_frame: torch.Tensor | None,
+                  *,
+                  p: Package,
+                  netG: BVDNet,
+                  gpu_id: int = 0) -> tuple[torch.Tensor | None,
+                                            np.ndarray,
+                                            np.ndarray]:
+    img_origin = p.img_origin
+    img_pool = p.img_pool
+    input_stream = []
+    for pos in FRAME_POS:
+        input_stream.append(impro.resize(
+            img_pool[pos][y-size:y+size, x-size:x+size], INPUT_SIZE, interpolation=cv2.INTER_CUBIC)[:, :, ::-1])
+
+    if previous_frame is None:
+        previous_frame = data.im2tensor(
+            input_stream[N], bgr2rgb=True, gpu_id=str(gpu_id))
+
+    input_stream = np.array(input_stream).reshape(
+        1, T, INPUT_SIZE, INPUT_SIZE, 3).transpose((0, 4, 1, 2, 3))
+    input_stream = data.to_tensor(
+        data.normalize(input_stream), gpu_id=gpu_id)
+
+    with torch.no_grad():
+        unmosaic_pred = netG(input_stream, previous_frame)
+
+    img_fake = data.tensor2im(unmosaic_pred, rgb2bgr=True)
+    previous_frame = unmosaic_pred
+
+    return (previous_frame, img_origin.copy(), img_fake.copy())
