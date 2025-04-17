7ba7bc3a097a9f5ac925c8c2ec5bf8472bc0fdda
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-18 02:41:53 +0800

    types, vars name

--- mosaic/free/cleaner/processor/position.py
+++ mosaic/free/cleaner/processor/position.py
@@ -3,27 +3,32 @@ from typing import Any
 import numpy as np
 
 import mosaic.free.utils.image_processing as impro
+from mosaic.free.net.netM.BiSeNet import BiSeNet
 from mosaic.free.utils import data
 
 
-def run_segment(img, net, size=360, gpu_id='-1'):
+def run_segment(img: np.ndarray,
+                netM: BiSeNet,
+                size: int = 360,
+                gpu_id: str = '-1'):
     img = impro.resize(img, size)
-    img = data.im2tensor(img, gpu_id=gpu_id, bgr2rgb=False, is0_1=True)
-    mask = net(img)
+    img_tensor = data.im2tensor(img, gpu_id=gpu_id, bgr2rgb=False, is0_1=True)
+    mask = netM(img_tensor)
     mask = data.tensor2im(mask, gray=True, is0_1=True)
     return mask
 
 
-def get_mosaic_position(img_origin,
-                        net_mosaic_pos,
+def get_mosaic_position(img_origin: np.ndarray,
+                        netM: BiSeNet,
                         all_mosaic_area: bool = False,
                         mask_threshold: int = 64,
                         ex_mult: float = 1.5) -> tuple[int, int, int, Any]:
     h, w = img_origin.shape[:2]
-    mask = run_segment(img_origin, net_mosaic_pos, size=360, gpu_id='0')
+    mask = run_segment(img_origin, netM, size=360, gpu_id='0')
     # mask_1 = mask.copy()
-    mask = impro.mask_threshold(mask, ex_mun=int(
-        min(h, w)/20), threshold=mask_threshold)
+    mask = impro.mask_threshold(mask,
+                                ex_mun=int(min(h, w)/20),
+                                threshold=mask_threshold)
     if not all_mosaic_area:
         mask = impro.find_mostlikely_ROI(mask)
     x, y, size, _area = impro.boundingSquare(mask, Ex_mul=ex_mult)
