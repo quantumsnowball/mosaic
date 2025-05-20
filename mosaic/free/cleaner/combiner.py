88cfe8a05dede218f2eeb92c1cbf23e34b011535
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-20 21:48:44 +0800

    move stream into run()

--- mosaic/free/cleaner/combiner.py
+++ mosaic/free/cleaner/combiner.py
@@ -15,22 +15,9 @@ class Combiner:
         output_file: Path,
         raw_info: bool,
     ) -> None:
-        self.origin = s = source.origin
+        self.origin = source.origin
         self._input = source
-        self._stream = (
-            ffmpeg.output(
-                ffmpeg.input(str(self.input),
-                             format='rawvideo',
-                             pix_fmt='rgb24',
-                             s=f'{s.width}x{s.height}',
-                             framerate=s.framerate).video,
-                ffmpeg.input(str(s), **s.ffmpeg_input_kwargs).audio,
-                str(output_file),
-                vcodec='libx264',
-                pix_fmt='yuv420p')
-            .global_args('-hide_banner')
-            .overwrite_output()
-        )
+        self._output_file = output_file
         self._proc: Popen | None = None
         self._pbar = None if raw_info else ProgressBar(self.origin.duration)
 
@@ -45,7 +32,24 @@ class Combiner:
         pass
 
     def run(self) -> None:
-        self._proc = self._stream.run_async()
+        # create the ffmpeg stream command using correct info
+        stream = (
+            ffmpeg.output(
+                ffmpeg.input(str(self.input),
+                             format='rawvideo',
+                             pix_fmt='rgb24',
+                             s=f'{self.origin.width}x{self.origin.height}',
+                             framerate=self.origin.framerate).video,
+                ffmpeg.input(str(self.origin), **self.origin.ffmpeg_input_kwargs).audio,
+                str(self._output_file),
+                vcodec='libx264',
+                pix_fmt='yuv420p')
+            .global_args('-hide_banner')
+            .overwrite_output()
+        )
+
+        # run
+        self._proc = stream.run_async()
 
     def wait(self) -> None:
         assert self._proc is not None
