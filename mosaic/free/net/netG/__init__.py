9243c6eb532b65dacecef5f142ed07aa5bab1fed
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-20 21:54:33 +0800

    silent unnecessary infos, cleaner pbar

--- mosaic/free/net/netG/__init__.py
+++ mosaic/free/net/netG/__init__.py
@@ -9,7 +9,7 @@ from mosaic.free.net.netG.BVDNet import show_paramsnumber
 
 def video(model_path: Path) -> BVDNet:
     netG = video_G(N=2, n_blocks=4)
-    show_paramsnumber(netG, 'netG')
+    # show_paramsnumber(netG, 'netG')
     # netG.load_state_dict(torch.load(opt.model_path))
     netG.load_state_dict(torch.load(model_path))
     netG.cuda()
