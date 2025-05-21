63cbe60fbe4e0f751507a850e81a425447560859
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-22 00:04:19 +0800

    create accept input and output file as arg

--- mosaic/jobs/create.py
+++ mosaic/jobs/create.py
@@ -1,6 +1,15 @@
+from pathlib import Path
+
 import click
+
+from mosaic.utils import VideoPathParamType
 
 
 @click.command
-def create() -> None:
-    print('create a new job')
+@click.option('-i', '--input-file', required=True, type=VideoPathParamType(), help='input media path')
+@click.argument('output-file', required=True, type=VideoPathParamType())
+def create(
+    input_file: Path,
+    output_file: Path,
+) -> None:
+    print(f'mosaic jobs create -i {input_file} {output_file}')
