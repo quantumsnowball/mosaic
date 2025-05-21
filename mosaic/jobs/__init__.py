8a35d8c9b146032525b3be8892e237eef58268c9
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-22 00:52:52 +0800

    jobs list jobs

--- mosaic/jobs/__init__.py
+++ mosaic/jobs/__init__.py
@@ -1,15 +1,19 @@
 import click
 
 from mosaic.jobs.create import create
+from mosaic.jobs.utils import MOSAIC_TEMP_DIR
 
 
 @click.group(invoke_without_command=True)
 @click.pass_context
 def jobs(ctx: click.Context) -> None:
+    # jobs can be a standalone command
     if ctx.invoked_subcommand:
         return
 
-    print('list all jobs found in the cwd')
+    for dirpath in MOSAIC_TEMP_DIR.glob('./*/'):
+        dirname = dirpath.name
+        print(dirname)
 
 
 jobs.add_command(create)
