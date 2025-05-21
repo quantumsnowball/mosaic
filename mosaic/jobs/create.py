bebb57cf7bf5059affd0c6c1cf933ae790338c64
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-22 00:07:01 +0800

    create free as a subcommand, later can add create upscale as another

--- mosaic/jobs/create.py
+++ mosaic/jobs/create.py
@@ -5,11 +5,16 @@ import click
 from mosaic.utils import VideoPathParamType
 
 
-@click.command
+@click.group
+def create() -> None:
+    pass
+
+
+@create.command
 @click.option('-i', '--input-file', required=True, type=VideoPathParamType(), help='input media path')
 @click.argument('output-file', required=True, type=VideoPathParamType())
-def create(
+def free(
     input_file: Path,
     output_file: Path,
 ) -> None:
-    print(f'mosaic jobs create -i {input_file} {output_file}')
+    print(f'mosaic jobs create free -i {input_file} {output_file}')
