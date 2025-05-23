2f639765e20c9dee84d1ac4ead5b1114cff31c63
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-24 01:04:22 +0800

    apply: free.combiner

--- mosaic/free/cleaner/combiner.py
+++ mosaic/free/cleaner/combiner.py
@@ -2,9 +2,8 @@ from pathlib import Path
 from subprocess import Popen
 from typing import Self
 
-import ffmpeg
-
 from mosaic.free.cleaner.processor import Processor
+from mosaic.utils.ffmpeg import FFmpeg
 from mosaic.utils.progress import ProgressBar
 
 
@@ -36,30 +35,44 @@ class Combiner:
 
     def run(self) -> None:
         # create the ffmpeg stream command using correct info
-        stream = (
-            ffmpeg.output(
-                ffmpeg.input(str(self.input),
-                             format='rawvideo',
-                             pix_fmt='rgb24',
-                             s=f'{self.origin.width}x{self.origin.height}',
-                             framerate=self.origin.framerate).video,
-                ffmpeg.input(str(self.origin), **self.origin.ffmpeg_input_kwargs).audio,
+        ffmpeg = (
+            FFmpeg()
+            .global_args(
+                '-hide_banner',
+                '-y',
+            )
+            .input(
+                # 0:v
+                '-f', 'rawvideo',
+                '-framerate', self.origin.framerate,
+                '-pix_fmt', 'rgb24',
+                '-s', f'{self.origin.width}x{self.origin.height}',
+                '-i', str(self.input),
+                # 1:a
+                *self.origin.ffmpeg_input_args,
+                '-i', str(self.origin),
+            )
+            .output(
+                '-map', '0:v',
+                '-map', '1:a',
+                #
+                '-pix_fmt', 'yuv420p',
+                '-vcodec', 'libx264',
                 str(self._output_file),
-                vcodec='libx264',
-                pix_fmt='yuv420p')
-            .global_args('-hide_banner')
-            .overwrite_output()
+            )
         )
 
         # if not showing raw info, ask ffmpeg to print progress info and run progress bar
         if self._pbar:
-            stream = stream.global_args('-loglevel', 'fatal')
-            stream = stream.global_args('-progress', self._pbar.input)
-            stream = stream.global_args('-stats_period', ProgressBar.REFRESH_RATE)
+            ffmpeg.global_args(
+                '-loglevel', 'fatal',
+                '-progress', str(self._pbar.input),
+                '-stats_period', ProgressBar.REFRESH_RATE,
+            )
             self._pbar.run()
 
         # run
-        self._proc = stream.run_async()
+        self._proc = Popen(ffmpeg.args)
 
     def wait(self) -> None:
         assert self._proc is not None
