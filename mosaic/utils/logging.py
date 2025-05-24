8d5fe61611835a43f22688cfb394480c052f1ea4
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-24 19:22:13 +0800

    decorator maker: log_at_level(level) -> Wrapper

--- mosaic/utils/logging.py
+++ mosaic/utils/logging.py
@@ -1,7 +1,8 @@
 import functools
 import logging
 import os
-from typing import Callable, ParamSpec, TypeVar
+from logging import CRITICAL, DEBUG, ERROR, INFO, NOTSET, WARNING
+from typing import Callable, Literal, ParamSpec, TypeVar
 
 P = ParamSpec("P")
 R = TypeVar("R")
@@ -27,21 +28,28 @@ logger = logging.getLogger(__name__)
 
 
 # decorator
-def log(func: ToBeWrapped[P, R]) -> Wrapped[P, R]:
-    # wrapped function
-    @functools.wraps(func)
-    def wrapped(*args: P.args, **kwargs: P.kwargs) -> R:
-        # log info before funning the function
-        logger.info(f"CALLED >> {func.__module__}.{func.__name__}()")
+def log_at_level(level: int) -> Wrapper[P, R]:
+    def wrapper(func: ToBeWrapped[P, R]) -> Wrapped[P, R]:
+        # wrapped function
+        @functools.wraps(func)
+        def wrapped(*args: P.args, **kwargs: P.kwargs) -> R:
+            # log info before funning the function
+            logger.log(level, f"CALLED >> {func.__module__}.{func.__name__}()")
 
-        # run the actual function
-        result = func(*args, **kwargs)
+            # run the actual function
+            result = func(*args, **kwargs)
 
-        # log info after funning the function
-        logger.info(f"          {func.__module__}.{func.__name__}() >> RETURN")
+            # log info after funning the function
+            logger.log(level, f"          {func.__module__}.{func.__name__}() >> RETURN")
 
-        # return the run result
-        return result
+            # return the run result
+            return result
 
-    # return the wrapped function
-    return wrapped
+        # return the wrapped function
+        return wrapped
+
+    # return the wrapper
+    return wrapper
+
+
+log = log_at_level(INFO)
