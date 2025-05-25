9150605a3bf349b604e17c8658c029e4a941f3f7
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-26 04:12:43 +0800

    dynamic creation of create <command>

--- mosaic/jobs/create.py
+++ mosaic/jobs/create.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Literal
 
 import click
 
@@ -6,27 +7,36 @@ from mosaic.jobs.job import Job
 from mosaic.utils import VideoPathParamType
 from mosaic.utils.service import service
 
+CommandName = Literal['free', 'upscale']
+
 
 @click.group
 def create() -> None:
     pass
 
 
-@create.command
-@click.option('-i', '--input-file', required=True, type=VideoPathParamType(), help='input media path')
-@click.argument('output-file', required=True, type=VideoPathParamType())
-@service()
-def free(
-    input_file: Path,
-    output_file: Path,
-) -> None:
-    # create a new job
-    with Job.create(
-        command='free',
-        input_file=input_file,
-        output_file=output_file,
-    ) as job:
-        # save
-        job.save()
-        # run
-        job.run()
+def make_command(name: CommandName) -> click.Command:
+    @create.command(name=name)
+    @click.option('-i', '--input-file', required=True, type=VideoPathParamType(), help='input media path')
+    @click.argument('output-file', required=True, type=VideoPathParamType())
+    @service()
+    def command(
+        input_file: Path,
+        output_file: Path,
+    ) -> None:
+        # create a new job
+        with Job.create(
+            command=name,
+            input_file=input_file,
+            output_file=output_file,
+        ) as job:
+            # save
+            job.save()
+            # run
+            job.run()
+
+    return command
+
+
+# free
+make_command('free')
