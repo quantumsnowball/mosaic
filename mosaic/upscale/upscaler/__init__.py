1d0a4c021864aa31652bd497d295274141b73c3b
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-26 03:12:32 +0800

    Upscaler class signature

--- mosaic/upscale/upscaler/__init__.py
+++ mosaic/upscale/upscaler/__init__.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Self
 
 from mosaic.upscale.net.real_esrgan import RealESRGANer
 from mosaic.upscale.upscaler.combiner import Combiner
@@ -7,6 +8,44 @@ from mosaic.upscale.upscaler.splitter import Splitter
 from mosaic.utils import HMS
 from mosaic.utils.logging import trace
 from mosaic.utils.spec import VideoDest, VideoSource
+
+
+class Upscaler:
+    def __init__(
+        self,
+        input_file: Path,
+        start_time: HMS | None,
+        end_time: HMS | None,
+        scale: str,
+        output_file: Path,
+        raw_info: bool,
+        upsampler: RealESRGANer,
+    ) -> None:
+        pass
+
+    @trace
+    def __enter__(self) -> Self:
+        return self
+
+    @trace
+    def __exit__(self, type, value, traceback) -> None:
+        pass
+
+    @trace
+    def start(self) -> None:
+        pass
+
+    @trace
+    def run(self) -> None:
+        pass
+
+    @trace
+    def wait(self) -> None:
+        pass
+
+    @trace
+    def stop(self) -> None:
+        pass
 
 
 @trace
