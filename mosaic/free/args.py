32d9e80834e8a87504ad8118ee7d404fa31c35ba
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-26 20:44:19 +0800

    correct logics

--- mosaic/free/args.py
+++ mosaic/free/args.py
@@ -19,21 +19,33 @@ def preprocess(func: ToBeWrapped[P, R]) -> Wrapped[P, R]:
     @wraps(func)
     def wrapped(*args: P.args, **kwargs: P.kwargs) -> R | None:
         # extract
-        input_file = cast(Path, kwargs['input_file'])
-        start_time = cast(HMS, kwargs['start_time'])
-        end_time = cast(HMS, kwargs['end_time'])
-        force = cast(bool, kwargs['force'])
-        time_tag = cast(bool, kwargs['time_tag'])
-        output_file = cast(Path, kwargs['output_file'])
 
         # verify inputs
+        input_file = cast(Path, kwargs['input_file'])
         if not input_file.exists():
             click.secho(f'Input file does not exists: -i {input_file}', fg='red')
             return
+
+        # verify time
+        start_time = cast(HMS, kwargs['start_time'])
+        end_time = cast(HMS, kwargs['end_time'])
         if start_time and end_time:
             if not end_time > start_time:
                 click.secho(f'Invalid start time or end time: -ss {start_time}, -to {end_time}', fg='red')
                 return
+
+        # modify output filename
+        time_tag = cast(bool, kwargs['time_tag'])
+        output_file = cast(Path, kwargs['output_file'])
+        if time_tag:
+            kwargs['output_file'] = output_file.with_stem(
+                f'{output_file.stem}--'
+                f'{start_time.time_tag if start_time else ""}-'
+                f'{end_time.time_tag if end_time else ""}')
+
+        # prompt for output overwrite
+        force = cast(bool, kwargs['force'])
+        output_file = cast(Path, kwargs['output_file'])
         if not force and output_file.exists():
             if click.prompt(
                 click.style(f'Output file {output_file} already exist, overwrite? y/[N]', fg='red'),
@@ -42,13 +54,6 @@ def preprocess(func: ToBeWrapped[P, R]) -> Wrapped[P, R]:
                 show_default=False,
             ).lower() != 'y':
                 return
-
-        # modify output filename
-        if time_tag:
-            output_file = output_file.with_stem(
-                f'{output_file.stem}--'
-                f'{start_time.time_tag if start_time else ""}-'
-                f'{end_time.time_tag if end_time else ""}')
 
         # remove unwanted args
         kwargs.pop('force')
