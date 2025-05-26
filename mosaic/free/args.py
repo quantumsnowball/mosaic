da23d350f8dae86ca7cfe0effcf23725ba82b20d
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-26 20:23:57 +0800

    copy original logics

--- mosaic/free/args.py
+++ mosaic/free/args.py
@@ -1,5 +1,8 @@
 from functools import wraps
-from typing import Callable, ParamSpec, TypeVar
+from pathlib import Path
+from typing import Callable, ParamSpec, TypeVar, cast
+
+from mosaic.utils.time import HMS
 
 P = ParamSpec("P")
 R = TypeVar("R")
@@ -13,10 +16,29 @@ def preprocess(func: ToBeWrapped[P, R]) -> Wrapped[P, R]:
     # wrapped function
     @wraps(func)
     def wrapped(*args: P.args, **kwargs: P.kwargs) -> R | None:
+        # extract
+        input_file = cast(Path, kwargs['input_file'])
+        start_time = cast(HMS, kwargs['start_time'])
+        end_time = cast(HMS, kwargs['end_time'])
+        force = cast(bool, kwargs['force'])
+        time_tag = cast(bool, kwargs['time_tag'])
+        output_file = cast(Path, kwargs['output_file'])
 
-        # checking here
-        print(kwargs)
-        print('check and modify kwargs here')
+        # verify inputs
+        assert input_file.exists(), f'{input_file} does not exists'
+        if start_time and end_time:
+            if not end_time > start_time:
+                raise ValueError('Invalid start time or end time')
+        if not force and output_file.exists():
+            if input(f'Output file {output_file} already exist, overwrite? y/[N] ').lower() != 'y':
+                return
+
+        # modify output filename
+        if time_tag:
+            output_file = output_file.with_stem(
+                f'{output_file.stem}--'
+                f'{start_time.time_tag if start_time else ""}-'
+                f'{end_time.time_tag if end_time else ""}')
 
         # run the actual function, return as is
         return func(*args, **kwargs)
