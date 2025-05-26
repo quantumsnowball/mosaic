c840d1971ee4e43a71de8d37e9e3ca24d6baea18
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-26 23:09:56 +0800

    dim finished job

--- mosaic/jobs/menu.py
+++ mosaic/jobs/menu.py
@@ -13,8 +13,9 @@ def title(
     *,
     newline: bool = True,
     fg: str = 'yellow',
+    dim: bool = False,
 ) -> str:
-    txt = style(f'{i+1}: {job.timestamp_pp} - {job.id}', fg=fg)
+    txt = style(f'{i+1}: {job.timestamp_pp} - {job.id}', fg=fg, dim=dim)
     if newline:
         txt += '\n'
     return txt
@@ -28,12 +29,27 @@ def field(
     newline: bool = True,
     fg_key: str = 'cyan',
     fg_val: str = 'green',
+    dim: bool = False,
 ) -> str:
-    txt_key = style(f'{key:>{width}s}', fg=fg_key)
-    txt_val = style(f'{str(val)}', fg=fg_val)
+    txt_key = style(f'{key:>{width}s}', fg=fg_key, dim=dim)
+    txt_val = style(f'{str(val)}', fg=fg_val, dim=dim)
     txt = f'{txt_key}: {txt_val}'
     if newline:
         txt += '\n'
+    return txt
+
+
+def job_info(i: int, job: Job) -> str:
+    dim = job.checklist.is_finished
+    txt = title(i, job, dim=dim)
+    for key, val in {
+        'progress': f'{job.checklist.count_finished} / {job.checklist.count} completed',
+        'command': job.command,
+        'segment time': job.segment_time,
+        'input file': job.input_file,
+        'output file': job.output_file,
+    }.items():
+        txt += field(key, val, dim=dim)
     return txt
 
 
@@ -45,14 +61,7 @@ class Menu:
 
     def list_jobs(self) -> None:
         for i, job in enumerate(self.jobs):
-            click.echo((
-                title(i, job) +
-                field('progress', f'{job.checklist.count_finished} / {job.checklist.count} completed') +
-                field('command', job.command) +
-                field('segment time', job.segment_time) +
-                field('input file', job.input_file) +
-                field('output file', job.output_file)
-            ))
+            click.echo(job_info(i, job))
 
     def prompt(self) -> None:
         if len(self.jobs) > 0:
