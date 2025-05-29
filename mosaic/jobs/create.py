644a7a0327e0ab213ea4383eb99c19c1057edaee
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-29 18:33:42 +0800

    rewrite click command free and copy, args wrapper can reuse as need

--- mosaic/jobs/create.py
+++ mosaic/jobs/create.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import click
 
 from mosaic.jobs.job import create_job
-from mosaic.jobs.utils import Command
 from mosaic.utils.path import PathParamType
 from mosaic.utils.service import service
 from mosaic.utils.time import HMS, HMSParamType
@@ -14,32 +13,70 @@ def create() -> None:
     pass
 
 
-def make_command(name: Command) -> click.Command:
-    @create.command(name=name)
-    @click.option('-i', '--input-file', required=True, type=PathParamType(), help='input media path')
-    @click.option('-sg', '--segment-time', required=False, default='00:05:00', type=HMSParamType(), help='segment time')
-    @click.argument('output-file', required=True, type=PathParamType())
-    @service()
-    def command(
-        input_file: Path,
-        segment_time: HMS,
-        output_file: Path,
-    ) -> None:
-        # create a new job
-        with create_job(
-            command=name,
-            segment_time=segment_time,
-            input_file=input_file,
-            output_file=output_file,
-        ) as job:
-            # save
-            job.save()
-            # run
-            job.run()
-
-    return command
+class args:
+    input_file = click.option(
+        '-i',
+        '--input-file',
+        required=True,
+        type=PathParamType(),
+        help='input media path'
+    )
+    segment_time = click.option(
+        '-sg',
+        '--segment-time',
+        required=False,
+        default='00:05:00',
+        type=HMSParamType(),
+        help='segment time'
+    )
+    output_file = click.argument(
+        'output-file',
+        required=True,
+        type=PathParamType()
+    )
 
 
-# free
-make_command('free')
-make_command('copy')
+@create.command
+@args.input_file
+@args.segment_time
+@args.output_file
+@service()
+def free(
+    input_file: Path,
+    segment_time: HMS,
+    output_file: Path,
+) -> None:
+    # create a new job
+    with create_job(
+        command='free',
+        segment_time=segment_time,
+        input_file=input_file,
+        output_file=output_file,
+    ) as job:
+        # save
+        job.save()
+        # run
+        job.run()
+
+
+@create.command
+@args.input_file
+@args.segment_time
+@args.output_file
+@service()
+def copy(
+    input_file: Path,
+    segment_time: HMS,
+    output_file: Path,
+) -> None:
+    # create a new job
+    with create_job(
+        command='copy',
+        segment_time=segment_time,
+        input_file=input_file,
+        output_file=output_file,
+    ) as job:
+        # save
+        job.save()
+        # run
+        job.run()
