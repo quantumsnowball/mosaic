fb0354dee56216654a4aee1617b7964cad5a9019
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-29 17:17:26 +0800

    rewrite load_job helper

--- mosaic/jobs/job/__init__.py
+++ mosaic/jobs/job/__init__.py
@@ -2,7 +2,7 @@ import json
 import shutil
 from datetime import datetime
 from pathlib import Path
-from typing import Self
+from typing import Self, cast
 from uuid import UUID, uuid4
 
 import mosaic.jobs.job.base
@@ -230,6 +230,40 @@ def create_job(
             command=command,
             id=uuid4(),
             timestamp=datetime.now(),
+            segment_time=segment_time,
+            input_file=input_file,
+            output_file=output_file,
+        )
+    elif command == 'upscale':
+        raise NotImplementedError
+
+
+def load_job(
+    dirpath: Path
+) -> mosaic.jobs.job.base.Job:
+    fpath = dirpath / mosaic.jobs.job.base.Job.info_fname
+    with open(fpath, 'r') as f:
+        d = json.load(f)
+    command: Command = cast(Command, d['command'])
+    id = UUID(d['id'])
+    timestamp = datetime.fromisoformat(d['timestamp'])
+    segment_time = HMS.from_str(d['segment_time'])
+    input_file = Path(d['input_file'])
+    output_file = Path(d['output_file'])
+    if command == 'free':
+        return FreeJob(
+            command=command,
+            id=id,
+            timestamp=timestamp,
+            segment_time=segment_time,
+            input_file=input_file,
+            output_file=output_file,
+        )
+    elif command == 'copy':
+        return CopyJob(
+            command=command,
+            id=id,
+            timestamp=timestamp,
             segment_time=segment_time,
             input_file=input_file,
             output_file=output_file,
