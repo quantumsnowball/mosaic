ac927cd8df4b36b730cab58a3a9ed1dad911fa43
Quantum Snowball <quantum.snowball@gmail.com>
2025-08-28 19:56:43 +0800

    apply FreeJobSave

--- mosaic/jobs/job/free.py
+++ mosaic/jobs/job/free.py
@@ -1,4 +1,5 @@
 import json
+from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
 from typing import Self, override
@@ -9,10 +10,15 @@ import click
 from mosaic.free.cleaner import Cleaner
 from mosaic.free.net.netG import video
 from mosaic.free.net.netM import bisenet
-from mosaic.jobs.job.base import Job
+from mosaic.jobs.job.base import Job, Save
 from mosaic.utils import PACKAGE_ROOT
 from mosaic.utils.logging import log
 from mosaic.utils.time import HMS
+
+
+@dataclass
+class FreeJobSave(Save):
+    pass
 
 
 class FreeJob(Job):
@@ -44,14 +50,14 @@ class FreeJob(Job):
     @override
     def save(self) -> None:
         info_fpath = self.job_dirpath / self.info_fname
-        info = {k: str(v) for k, v in dict(
+        info = FreeJobSave(
             command=self.command,
             id=self.id,
             timestamp=self.timestamp_iso,
             segment_time=self.segment_time,
             input_file=self.input_file,
             output_file=self.output_file,
-        ).items()}
+        ).dict
         with open(info_fpath, 'w') as f:
             json.dump(info, f, indent=4)
 
