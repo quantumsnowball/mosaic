184db88fa7a9a650e786cf20fed8f0cd6ec5c1b5
Quantum Snowball <quantum.snowball@gmail.com>
2025-08-29 14:29:58 +0800

    user name as suffix to ROOT_DIR

--- mosaic/utils/__init__.py
+++ mosaic/utils/__init__.py
@@ -1,5 +1,6 @@
+from getpass import getuser
 from pathlib import Path
 
 PACKAGE_ROOT = Path(__file__).parent.parent
-ROOT_DIR = Path('.mosaic')
+ROOT_DIR = Path(f'.mosaic.{getuser()}')
 TEMP_DIR = ROOT_DIR / '.temp'
