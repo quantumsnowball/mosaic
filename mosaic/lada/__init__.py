b9be285abd5275db7abe7543e51e90c0f4bba289
Quantum Snowball <quantum.snowball@gmail.com>
2025-10-31 02:36:51 +0800

    takes the same args as free sub-command

--- mosaic/lada/__init__.py
+++ mosaic/lada/__init__.py
@@ -1,10 +1,27 @@
+from pathlib import Path
+
 import click
 
+from mosaic.utils.path import PathParamType
 from mosaic.utils.service import service
+from mosaic.utils.time import HMS, HMSParamType
 
 
 @click.command()
 @service()
-def lada() -> None:
+@click.option('-i', '--input-file', required=True, type=PathParamType(), help='input media path')
+@click.option('-ss', '--start-time', default=None, type=HMSParamType(), help='start time in HH:MM:SS')
+@click.option('-to', '--end-time', default=None, type=HMSParamType(), help='end time in HH:MM:SS')
+# @click.option('-y', '--force', is_flag=True, default=False, help='overwrite output file without asking')
+# @click.option('--time-tag', is_flag=True, default=False, help='auto append time tag at end of filename')
+@click.option('--raw-info', is_flag=True, default=False, help='display raw ffmpeg info')
+@click.argument('output-file', required=True, type=PathParamType())
+def lada(
+    input_file: Path,
+    start_time: HMS | None,
+    end_time: HMS | None,
+    raw_info: bool,
+    output_file: Path,
+) -> None:
     # run
-    print('lada sub-command')
+    print(f'{start_time=} {end_time=} {raw_info=} {input_file=} {output_file=}')
