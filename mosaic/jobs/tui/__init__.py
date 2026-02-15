82dfdb4b238765c6139f0a4c1033c14f9dc22a67
Quantum Snowball <quantum.snowball@gmail.com>
2026-02-16 03:11:22 +0800

    a better UI with Vertical Container with some basic style

--- mosaic/jobs/tui/__init__.py
+++ mosaic/jobs/tui/__init__.py
@@ -1,4 +1,5 @@
 from textual.app import App, ComposeResult
+from textual.containers import Vertical
 from textual.widgets import Footer, Header, Label, ListItem, ListView
 
 from mosaic.jobs.manager import Manager
@@ -8,14 +9,29 @@ from mosaic.jobs.text import job_info
 # 1. Define the TUI Layout
 class Dashboard(App):
     """A simple Textual TUI for managing jobs."""
+    CSS = """
+    #main-window {
+        border: round $primary;      /* The window frame */
+        border-title-align: center;  /* Center the title */
+        margin: 1 2;                 /* Breathing room from screen edges */
+        background: $surface;
+    }
+    
+    #job_list {
+        background: transparent;
+    }
+    """
+
     BINDINGS = [("q", "quit", "Quit")]
 
     def compose(self) -> ComposeResult:
         # header
         yield Header()
 
-        # Create a scrollable list of items
-        yield ListView(id="job_list")
+        # Create a Vertical container to be the main window frame
+        with Vertical(id='main-window') as v:
+            v.border_title = 'JOBS'
+            yield ListView(id='job_list')
 
         # footer
         yield Footer()
