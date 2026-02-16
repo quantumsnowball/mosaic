8d3a69b141e856d8c344386a9c2448bdf1ae3698
Quantum Snowball <quantum.snowball@gmail.com>
2026-02-17 05:10:09 +0800

    j k movement for FileTree

--- mosaic/jobs/tui/create/__init__.py
+++ mosaic/jobs/tui/create/__init__.py
@@ -3,10 +3,14 @@ from textual.containers import Vertical
 from textual.widgets import DirectoryTree
 
 
+class FileTree(DirectoryTree):
+    from .bindings import BINDINGS
+
+
 class FileList(Vertical):
     def __init__(self) -> None:
         super().__init__(classes='section')
-        self.directory_tree = DirectoryTree('./', id='directory-tree')
+        self.directory_tree = FileTree('./', id='directory-tree')
 
     def compose(self) -> ComposeResult:
         yield self.directory_tree
---
+++ mosaic/jobs/tui/create/bindings.py
@@ -0,0 +1,6 @@
+from textual.binding import BindingType
+
+BINDINGS: list[BindingType] = [
+    ("k", "cursor_up", "Up"),
+    ("j", "cursor_down", "Down"),
+]
