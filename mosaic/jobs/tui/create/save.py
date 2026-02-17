31bd1ef3ea623060a62482f49e7572fd908a833b
Quantum Snowball <quantum.snowball@gmail.com>
2026-02-17 19:25:34 +0800

    factor out styles

--- mosaic/jobs/tui/create/save.py
+++ mosaic/jobs/tui/create/save.py
@@ -8,6 +8,7 @@ from textual.widgets import Input, Label
 
 class SaveAsModalScreen(ModalScreen[str]):
     from .bindings import save as BINDINGS
+    from .styles import save as CSS
 
     def __init__(self, default_path: Path) -> None:
         super().__init__()
@@ -33,27 +34,3 @@ class SaveAsModalScreen(ModalScreen[str]):
     def action_cancel(self) -> None:
         # press excape to cancel
         self.dismiss(None)
-
-    CSS = """
-    SaveAsModalScreen {
-        align: center middle;
-        background: $background 50%;
-    }
-    #modal-container {
-        width: 60;
-        height: auto;
-        padding: 1 2;
-        background: $surface;
-        border: thick $primary;
-    }
-    #output-input {
-        margin: 1 0;
-    }
-    #modal-buttons {
-        height: auto;
-        align: right middle;
-    }
-    #modal-buttons Button {
-        margin-left: 1;
-    }
-    """
---
+++ mosaic/jobs/tui/create/styles.py
@@ -0,0 +1,23 @@
+save = """
+    SaveAsModalScreen {
+        align: center middle;
+        background: $background 50%;
+    }
+    #modal-container {
+        width: 60;
+        height: auto;
+        padding: 1 2;
+        background: $surface;
+        border: thick $primary;
+    }
+    #output-input {
+        margin: 1 0;
+    }
+    #modal-buttons {
+        height: auto;
+        align: right middle;
+    }
+    #modal-buttons Button {
+        margin-left: 1;
+    }
+"""
