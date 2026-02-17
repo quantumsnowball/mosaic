24cd1098f1b2da21b2cd52663277393b457f3c91
Quantum Snowball <quantum.snowball@gmail.com>
2026-02-17 15:27:04 +0800

    print file rel path

--- mosaic/jobs/tui/create/__init__.py
+++ mosaic/jobs/tui/create/__init__.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from textual.app import ComposeResult
 from textual.containers import Vertical
 from textual.widgets import DirectoryTree
@@ -5,6 +7,12 @@ from textual.widgets import DirectoryTree
 
 class FileTree(DirectoryTree):
     from .bindings import BINDINGS
+
+    def on_directory_tree_file_selected(self, event: DirectoryTree.FileSelected) -> None:
+        abs_path = event.path
+        rel_path = abs_path.relative_to(Path.cwd())
+        self.log(f"FILE SELECTED: {rel_path}")
+        self.app.notify(f"Selected: [bold cyan]{rel_path}[/]", title="File Browser")
 
 
 class FileList(Vertical):
